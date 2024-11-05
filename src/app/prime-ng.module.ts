459b8277ab956b7b3473fa01cddc52828ae94ee5
Kevin Aguilar <kevinv.aguilarl@gmail.com>
2024-11-05 11:04:57 -0500

    last commit

---
+++ src/app/prime-ng.module.ts
@@ -0,0 +1,221 @@
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipsModule } from 'primeng/chips';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { DropdownModule } from 'primeng/dropdown';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputOtpModule } from 'primeng/inputotp';
+import { KnobModule } from 'primeng/knob';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { ListboxModule } from 'primeng/listbox';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { PasswordModule } from 'primeng/password';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { TriStateCheckboxModule } from 'primeng/tristatecheckbox';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { DataViewModule } from 'primeng/dataview';
+import { OrderListModule } from 'primeng/orderlist';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { PaginatorModule } from 'primeng/paginator';
+import { PickListModule } from 'primeng/picklist';
+import { ScrollerModule } from 'primeng/scroller';
+import { TableModule } from 'primeng/table';
+import { TimelineModule } from 'primeng/timeline';
+import { TreeModule } from 'primeng/tree';
+import { TreeTableModule } from 'primeng/treetable';
+import { AccordionModule } from 'primeng/accordion';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { SplitterModule } from 'primeng/splitter';
+import { StepperModule } from 'primeng/stepper';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabViewModule } from 'primeng/tabview';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { DialogModule } from 'primeng/dialog';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { SidebarModule } from 'primeng/sidebar';
+import { TooltipModule } from 'primeng/tooltip';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MenuModule } from 'primeng/menu';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { InputIconModule } from 'primeng/inputicon';
+
+
+
+
+@NgModule({
+  imports:[
+    ButtonModule,
+    AutoCompleteModule,
+    CalendarModule,
+    CascadeSelectModule,
+    CheckboxModule,
+    ChipsModule,
+    ColorPickerModule,
+    DropdownModule,
+    FloatLabelModule,
+    IconFieldModule,
+    InputGroupModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    InputOtpModule,
+    KnobModule,
+    KeyFilterModule,
+    ListboxModule,
+    MultiSelectModule,
+    PasswordModule,
+    RadioButtonModule,
+    RatingModule,
+    SelectButtonModule,
+    SliderModule,
+    TreeSelectModule,
+    TriStateCheckboxModule,
+    ToggleButtonModule,
+    SplitButtonModule,
+    SpeedDialModule,
+    DataViewModule,
+    OrderListModule,
+    OrganizationChartModule,
+    PaginatorModule,
+    PickListModule,
+    ScrollerModule,
+    TableModule,
+    TimelineModule,
+    TreeModule,
+    TreeTableModule,
+    AccordionModule,
+    CardModule,
+    DividerModule,
+    FieldsetModule,
+    PanelModule,
+    SplitterModule,
+    StepperModule,
+    ScrollPanelModule,
+    TabViewModule,
+    ToolbarModule,
+    ConfirmDialogModule,
+    ConfirmPopupModule,
+    DialogModule,
+    DynamicDialogModule,
+    OverlayPanelModule,
+    SidebarModule,
+    TooltipModule,
+    AvatarModule,
+    AvatarGroupModule,
+    BadgeModule,
+    FormsModule,
+    MenuModule,
+    BadgeModule,
+    RippleModule,
+    AvatarModule,
+  ],
+  exports: [
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    //BrowserModule, 
+    //BrowserAnimationsModule,
+    ButtonModule,
+    AutoCompleteModule,
+    CalendarModule,
+    CascadeSelectModule,
+    CheckboxModule,
+    ChipsModule,
+    ColorPickerModule,
+    DropdownModule,
+    FloatLabelModule,
+    IconFieldModule,
+    InputGroupModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    InputOtpModule,
+    KnobModule,
+    KeyFilterModule,
+    ListboxModule,
+    MultiSelectModule,
+    PasswordModule,
+    RadioButtonModule,
+    RatingModule,
+    SelectButtonModule,
+    SliderModule,
+    TreeSelectModule,
+    TriStateCheckboxModule,
+    ToggleButtonModule,
+    SplitButtonModule,
+    SpeedDialModule,
+    DataViewModule,
+    OrderListModule,
+    OrganizationChartModule,
+    PaginatorModule,
+    PickListModule,
+    ScrollerModule,
+    TableModule,
+    TimelineModule,
+    TreeModule,
+    TreeTableModule,
+    AccordionModule,
+    CardModule,
+    DividerModule,
+    FieldsetModule,
+    PanelModule,
+    SplitterModule,
+    StepperModule,
+    ScrollPanelModule,
+    TabViewModule,
+    ToolbarModule,
+    ConfirmDialogModule,
+    ConfirmPopupModule,
+    DialogModule,
+    DynamicDialogModule,
+    OverlayPanelModule,
+    SidebarModule,
+    TooltipModule,
+    AvatarGroupModule,
+    BadgeModule,
+    MenuModule,
+    BadgeModule,
+    RippleModule,
+    AvatarModule,
+    IconFieldModule,
+    InputIconModule,
+    IconFieldModule,
+    PasswordModule
+    
+  
+  ], 
+  providers:[]
+})
+export class PrimeNgModule { }
